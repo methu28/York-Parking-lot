19493d9933dba1febec5e9ad57bfe253f5972318
methu28 <77935285+methu28@users.noreply.github.com>
2025-03-22 14:58:50 -0400

    Update ManagerUser.java

--- userParkingSystem/src/parkingSystem/model/ManagerUser.java
+++ userParkingSystem/src/parkingSystem/model/ManagerUser.java
@@ -3,7 +3,6 @@ package parkingSystem.model;
 public class ManagerUser extends AbstractUser {
 	public ManagerUser(String name, String email, String password) {
 		super(name, email, password, "Manager");
-		this.isValid = true; 
 		// TODO Auto-generated constructor stub
 	}
 }
