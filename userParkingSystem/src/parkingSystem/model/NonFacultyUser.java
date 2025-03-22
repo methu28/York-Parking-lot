cf5c721c399452a60759973c90c0b47195176a93
methu28 <77935285+methu28@users.noreply.github.com>
2025-03-21 22:15:45 -0400

    Delete userParkingSystem directory

--- userParkingSystem/src/parkingSystem/model/NonFacultyUser.java
+++
@@ -1,8 +0,0 @@
-package parkingSystem.model;
-
-public class NonFacultyUser extends AbstractUser {
-	public NonFacultyUser(String name, String email, String password) {
-		super(name, email, password, "Non-Faculty");
-		// TODO Auto-generated constructor stub
-	}
-}
--- userParkingSystem/src/parkingSystem/model/StudentUser.java
+++
@@ -1,9 +0,0 @@
-package parkingSystem.model;
-
-public class StudentUser extends AbstractUser {
-	public StudentUser(String name, String email, String password) {
-		super(name, email, password, "Student");
-		// TODO Auto-generated constructor stub
-	}
-
-}
