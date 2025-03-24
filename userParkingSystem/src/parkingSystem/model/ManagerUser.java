2a94a2f4d9a96da20210bc3302853fbe9fe4bcf6
methu28 <77935285+methu28@users.noreply.github.com>
2025-03-23 22:09:51 -0400

    Delete userParkingSystem directory

--- userParkingSystem/src/parkingSystem/model/ManagerUser.java
+++
@@ -1,8 +0,0 @@
-package parkingSystem.model;
-
-public class ManagerUser extends AbstractUser {
-	public ManagerUser(String name, String email, String password) {
-		super(name, email, password, "Manager");
-		// TODO Auto-generated constructor stub
-	}
-}
