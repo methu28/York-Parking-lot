ae82f16d87d67c16be0aaa8f0e610dc5226499a6
Muhammad Basit <139032097+Basit-m@users.noreply.github.com>
2025-03-24 01:24:59 -0400

    Update ParkingSpace.java

--- userParkingSystem/src/parkingSystem/model/parking/ParkingSpace.java
+++ userParkingSystem/src/parkingSystem/model/parking/ParkingSpace.java
@@ -1,1 +1,99 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ParkingSpace {
+
+    private int id;
+    private ParkingLot lot;
+    private boolean isOccupied;
+    private boolean isEnabled;
+
+    //Checks for conflicts with other bookings
+    private List<Booking> bookings;
+
+    public ParkingSpace(int id, ParkingLot lot) {
+        this.id = id;
+        this.lot = lot;
+
+        this.bookings = new ArrayList<>();
+
+        this.isEnabled = true;
+        this.isOccupied = false;
+    }
+
+    public boolean isAvailable(LocalDateTime start, LocalDateTime end) {
+
+        clearPastBookings();
+
+        for(Booking booking : bookings) {
+            if(end.isAfter(booking.getStartTime()) && start.isBefore(booking.getStartTime())) {
+                return false;
+            }
+            else if(start.isBefore(booking.getEndTime()) && end.isAfter(booking.getEndTime())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public LocalDateTime getNextAvailableTime(LocalDateTime start) {
+        
+        clearPastBookings();
+
+        LocalDateTime reqStart = start;
+
+        for(Booking booking : bookings) {
+            if(reqStart.isAfter(booking.getStartTime()) && reqStart.isBefore(booking.getEndTime())) {
+                reqStart = booking.getEndTime();
+                break;
+            }
+        }
+        return reqStart;
+
+    }
+    public void clearPastBookings() {
+
+        LocalDateTime now = LocalDateTime.now();
+
+        for(Booking booking : bookings) {
+            if(now.isAfter(booking.getEndTime()) && booking.isCheckedOut()) {
+                bookings.remove(booking);
+            }
+        }
+
+    }
+
+    public void addBooking(Booking booking) {
+        this.bookings.add(booking);
+    }
+    public void removeBooking(Booking booking) {
+        this.bookings.remove(booking);
+    }
+
+    public boolean isOccupied() {
+        return this.isOccupied;
+    }
+    public boolean isEnabled() {
+        return this.isEnabled;
+    }
+    public void disable() {
+        this.isEnabled = false;
+    }
+    public void enable() {
+        this.isEnabled = true;
+    }
+    public List<Booking> getBookings() {
+        return this.bookings;
+    }
+    public ParkingLot getLot() {
+        return this.lot;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public String toString() {
+        return "Lot " + id;
+    }
+
+}
