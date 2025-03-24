b1a563a9fb18e424f35f7a60b6705be62ff70665
Muhammad Basit <139032097+Basit-m@users.noreply.github.com>
2025-03-24 00:12:02 -0400

    Update PasswordVerifier.java

--- userParkingSystem/src/parkingSystem/model/util/PasswordVerifier.java
+++ userParkingSystem/src/parkingSystem/model/util/PasswordVerifier.java
@@ -1,1 +1,16 @@
+public class PasswordVerifier {
 
+    private PasswordVerifier() {}
+
+    public static boolean isStrongPassword(String password) {
+
+        boolean hasLower = password.matches(".*[a-z].*");
+        boolean hasUpper = password.matches(".*[A-Z].*");
+        boolean hasDigit = password.matches(".*[0-9].*");
+        boolean hasSymbol = password.matches(".*[^A-Za-z0-9].*");
+
+        return hasLower && hasUpper && hasDigit && hasSymbol;
+
+    }
+
+}
