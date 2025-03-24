592760c1ed657652eaea3a065e57ba19b87a15eb
Muhammad Basit <139032097+Basit-m@users.noreply.github.com>
2025-03-24 00:11:02 -0400

    Update EmailVerifier.java

--- userParkingSystem/src/parkingSystem/model/util/EmailVerifier.java
+++ userParkingSystem/src/parkingSystem/model/util/EmailVerifier.java
@@ -1,1 +1,11 @@
+public class EmailVerifier {
 
+    private EmailVerifier() {}
+
+    public static boolean isValidEmail(String email) {
+
+        return email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+
+    }
+
+}
