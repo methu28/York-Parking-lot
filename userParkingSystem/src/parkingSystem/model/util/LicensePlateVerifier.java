fe5f0eddddd2f5eb48713352bf481fa60e0f3644
Muhammad Basit <139032097+Basit-m@users.noreply.github.com>
2025-03-24 00:12:32 -0400

    Update LicensePlateVerifier.java

--- userParkingSystem/src/parkingSystem/model/util/LicensePlateVerifier.java
+++ userParkingSystem/src/parkingSystem/model/util/LicensePlateVerifier.java
@@ -1,1 +1,7 @@
+public class LicensePlateVerifier {
+    private LicensePlateVerifier() {}
 
+    public static boolean isValidLicense(String license) {
+        return license.matches("^[A-Z0-9]{3,4}[- ]?[A-Z0-9]{3,4}$");
+    }
+}
