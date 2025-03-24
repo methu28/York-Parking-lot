83b2fd0447c33ef2c65ba4597f0762552d73138c
Muhammad Basit <139032097+Basit-m@users.noreply.github.com>
2025-03-24 13:32:11 -0400

    Update FacultyUser.java

--- userParkingSystem/src/parkingSystem/model/FacultyUser.java
+++ userParkingSystem/src/parkingSystem/model/FacultyUser.java
@@ -7,4 +7,12 @@ public class FacultyUser extends AbstractUser {
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public double getRate() {
+		if(!isValid) {
+			return 15.00;
+		}
+		return 8;
+	}
+
 }
