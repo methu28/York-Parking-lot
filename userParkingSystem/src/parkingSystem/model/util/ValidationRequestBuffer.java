c6b5c8b0c09ca8a9a12cad3676e976df9f59666d
Muhammad Basit <139032097+Basit-m@users.noreply.github.com>
2025-03-24 00:18:54 -0400

    Update ValidationRequestBuffer.java

--- userParkingSystem/src/parkingSystem/model/util/ValidationRequestBuffer.java
+++ userParkingSystem/src/parkingSystem/model/util/ValidationRequestBuffer.java
@@ -1,1 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class ValidationRequestBuffer {
+
+    private static ValidationRequestBuffer instance;
+    private List<User> requests;
+
+    private ValidationRequestBuffer() {
+        this.requests = new ArrayList<>();
+    }
+    public static ValidationRequestBuffer getInstance() {
+        if(instance == null) {
+            instance = new ValidationRequestBuffer();
+        }
+        return instance;
+    }
+
+    public void addRequest(User user) {
+        requests.add(user);
+        //notify
+        notifyManager(user);
+    }
+    public void removeRequest(User user) {
+        requests.remove(user);
+    }
+    public List<User> getRequests() {
+        return this.requests;
+    }
+    public void setRequests(List<User> requests) {
+        this.requests = requests;
+    }
+    public void notifyManager(User user) {
+        //Notify managers for the new user that needs validation
+    }
+}
